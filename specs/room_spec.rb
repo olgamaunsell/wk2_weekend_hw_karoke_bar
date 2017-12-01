894ad948f5163474677670753fb1a0f6dc7cf477
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-12-01 14:44:07 +0000

    tests to check room has songs - passed

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -6,19 +6,31 @@ require_relative("../song.rb")
 class TestRoom < MiniTest::Test
 
   def setup
-    @songs = []
+    @no_songs = []
     @song1 = Song.new("I want to break free")
-
-    @room = Room.new(3, @songs)
+    @song2 = Song.new("Park Life")
+    @songs = [@song1, @song2]
+    @room1 = Room.new(3, @no_songs)
+    @room2 = Room.new(3, @songs)
   end
 
   def test_get_room_number
-    assert_equal(3, @room.number)
+    assert_equal(3, @room1.number)
   end
 
   def test_room_has__no_songs
     expected = 0
-    actual = @room.songs.count()
+    actual = @room1.songs.count()
     assert_equal(0, actual)
+  end
+
+  def test_room_has__two_songs
+    expected = 0
+    actual = @room2.songs.count()
+    assert_equal(2, actual)
+  end
+
+  def test_get_first_song_in_songs
+    assert_equal("I want to break free", @room2.songs.first().name())
   end
 end
