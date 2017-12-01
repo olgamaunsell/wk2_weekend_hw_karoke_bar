4bfa649bb8b339ab4389d96ccb26028b6ba93d6f
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-12-01 14:30:00 +0000

    test_room_has__no_songs - passed

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,15 +1,24 @@
 require ("MiniTest/autorun")
 require ("MiniTest/rg")
 require_relative("../room.rb")
+require_relative("../song.rb")
 
 class TestRoom < MiniTest::Test
 
   def setup
-    @room = Room.new(3)
+    @songs = []
+    @song1 = Song.new("I want to break free")
+
+    @room = Room.new(3, @songs)
   end
 
   def test_get_room_number
     assert_equal(3, @room.number)
   end
 
+  def test_room_has__no_songs
+    expected = 0
+    actual = @room.songs.count()
+    assert_equal(0, actual)
+  end
 end
