48578ba0a0037c241836353f7899ecd3b99b0369
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-12-01 14:56:14 +0000

    test_room_has__no_guests - passed

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -6,16 +6,28 @@ require_relative("../song.rb")
 class TestRoom < MiniTest::Test
 
   def setup
+    #setup to test zero guests and zero songs in room
+    @no_guests = []
     @no_songs = []
+    @room1 = Room.new(3, @no_guests, @no_songs)
+    #setup to test guests in room
+    @room2 = Room.new(3, @no_guests, @no_songs)
+    #setup to test songs in room
     @song1 = Song.new("I want to break free")
     @song2 = Song.new("Park Life")
     @songs = [@song1, @song2]
-    @room1 = Room.new(3, @no_songs)
-    @room2 = Room.new(3, @songs)
+    @room3 = Room.new(3, @no_guests, @songs)
+
   end
 
   def test_get_room_number
     assert_equal(3, @room1.number)
+  end
+
+  def test_room_has__no_guests
+    expected = 0
+    actual = @room1.guests.count()
+    assert_equal(0, actual)
   end
 
   def test_room_has__no_songs
@@ -26,11 +38,13 @@ class TestRoom < MiniTest::Test
 
   def test_room_has__two_songs
     expected = 0
-    actual = @room2.songs.count()
+    actual = @room3.songs.count()
     assert_equal(2, actual)
   end
 
   def test_get_first_song_in_songs
-    assert_equal("I want to break free", @room2.songs.first().name())
+    assert_equal("I want to break free", @room3.songs.first().name())
   end
+
+
 end
